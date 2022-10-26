07a8bc61c98ff9f6e85107160ea6e7d8f1171c22
Lorenzo <lorenzo.osimani@studio.unibo.it>
2022-10-26 16:20:10 +0200

    Added Coverage plugin, updated scala version, created demo test

--- build.sbt
+++ build.sbt
@@ -1,10 +1,12 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
-ThisBuild / scalaVersion := "3.1.1"
+ThisBuild / scalaVersion := "3.2.0"
 
 libraryDependencies += "com.github.sbt" % "junit-interface" % "0.13.3" % Test
 
 libraryDependencies += "org.scalafx" %% "scalafx" % "19.0.0-R30"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.14" % Test
 
 scalacOptions ++= Seq("-unchecked", "-deprecation", "-encoding", "utf8", "-feature")
 
---
+++ src/test/scala/DemoScalaTest.scala
@@ -0,0 +1,29 @@
+import example_prof.BTrees.Tree.{Branch, Leaf, count, find, size}
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.matchers.must.Matchers as MustMatchers
+
+
+class DemoScalaTest extends AnyFunSpec {
+
+  describe("A tree with 3 elements") {
+    val tree = Branch(Branch(Leaf(1), Leaf(2)), Leaf(1))
+
+    it ("should have 3 elements") {
+      assert(size(tree) == 3)
+    }
+
+    it ("should cointain 2 but not 3") {
+      assert(find(tree, 2) == true)
+      assert(find(tree, 3) == false)
+    }
+
+    it ("should have 1 element of 2 and 2 elements of 1") {
+      assertResult(2) {count(tree, 1)}
+      assertResult(1) {count(tree, 2)}
+      assertResult(0) {count(tree, 3)}
+    }
+
+
+  }
+}
