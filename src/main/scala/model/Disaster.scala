e1b32f41212f92fbe09f194a8ba1936d87b1ca81
AlbertoDonati <alberto.donati6@studio.unibo.it>
2022-11-07 21:04:42 +0000

    prove ref disaster

--- src/main/scala/model/Disaster.scala
+++ src/main/scala/model/Disaster.scala
@@ -2,50 +2,57 @@ package model
 
 /** Represent a Disaster */
 sealed trait Disaster:
+
+  def name: String
   def damage: Int
+
+  def probability: Int
 
   def extension: Option[Int]
 
   //nonnn sarebbe meglio che l engine decidesse le coordinate
   //override coordinates: engine probabilmente
-  def probability: Int
+
 
   def temperature: Option[Double]
 
   val coordinates: (Int, Int) = Option(x, y)
 
   case object Earthquake extends Disaster :
+    override val name: String = "Earthquake"
     override val damage: Int = 1000
     override val extension: Int = 3
     override val probability: Double = 0.5
+    override val coordinates: (Int, Int) = (50,50)
 
   case object Meteorite extends Disaster :
-    override val gravity: Int = 1
+    override val name: String = "Meteorite"
     override val damage: Int = 1000
     override val extension: Int = 3
     override val probability: Double = 0.5
+    override val coordinates: (Int, Int) = (50,50)
 
   //refactoring
   case class IceAge extends Disaster :
-    override val gravity: Int = 1
+    override val name: String = "IceAge"
     override val damage: Int = 1000
-    override val extension: Int = 3
     override val probability: Double = 0.5
     override val temperature: Int = 100000
 
   case class Drought extends Disaster :
-    override val gravity: Int = 11
+    override val name: String = "Drought"
     override val damage: Int = 100
-    override val extension: Int = 23
     override val probability: Double = 0.3
-    override val temperature: Option[Double] = 10000
+    override val temperature: Int = 10000
 
   override def toString: String = {
     super.toString +
-      "\n damage: " + damage +
-      "\n extension " + extension +
-      "\n coordinateX" + coordinates(x)
-    "\n coordinateY" + coordinates(y)
+      "\n name: " + name
+      "\n damage: " + damage
+        //if
+      //if(extension !== null) => "\n extension " + extension +
+      //"\n coordinateX" + coordinates(x)
+    //"\n coordinateY" + coordinates(y)
   }
   //cè un modo di creare un array per riprendere i danni creati
   //problema che aclune variabili devono stare entro un certo range
