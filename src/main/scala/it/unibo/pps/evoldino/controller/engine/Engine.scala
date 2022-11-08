eecfb637f12c49d726e31cb388278416e86a82b6
Lorenzo <lorenzo.osimani@studio.unibo.it>
2022-11-08 17:49:37 +0100

    Engine Pause and relative tests implemented

--- src/main/scala/it/unibo/pps/evoldino/controller/engine/Engine.scala
+++ src/main/scala/it/unibo/pps/evoldino/controller/engine/Engine.scala
@@ -2,6 +2,7 @@ package it.unibo.pps.evoldino.controller.engine
 
 import cats.effect.{ ExitCode, IO, IOApp, Temporal }
 import cats.effect.IO.unit
+import cats.effect.unsafe.implicits.global
 
 import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.FiniteDuration
@@ -10,19 +11,38 @@ import WorldHistory._
 
 object Engine {
 
+  private var iteration_speed = EngineConstants.iteration_ms_1x
+
+  private var paused = false
+
+  private var ended = false
+
+  def startSimulation(): Unit = simulationLoop().unsafeRunAndForget()
+
+  def endSimulation(): Unit = ended = true
+
+  def pauseSimulation(): Unit = paused = true
+
+  def unpauseSimulation(): Unit =
+    paused = false
+    simulationLoop().unsafeRunAndForget()
+
+  def changeSpeed(): Unit = iteration_speed match
+    case EngineConstants.iteration_ms_1x => iteration_speed = EngineConstants.iteration_ms_2x
+    case EngineConstants.iteration_ms_2x => iteration_speed = EngineConstants.iteration_ms_4x
+    case EngineConstants.iteration_ms_4x => iteration_speed = EngineConstants.iteration_ms_1x
+
   def simulationLoop(): IO[Unit] = for {
-    _ <- Temporal[IO].sleep(
-      FiniteDuration.apply(EngineConstants.iteration_ms_1x, TimeUnit.MILLISECONDS)
-    )
+    _ <- Temporal[IO].sleep(FiniteDuration.apply(iteration_speed, TimeUnit.MILLISECONDS))
     _ <- nextIteration()
     _ <- dinosaursEatingPhase()
     _ <- applyDisturbances()
     _ <-
-      if (!hasSimulationEnded()) simulationLoop()
+      if (!hasSimulationEnded() && !paused) simulationLoop()
       else unit
   } yield ()
 
-  private def hasSimulationEnded(): Boolean = false
+  private def hasSimulationEnded(): Boolean = false || ended
 
   given Conversion[Unit, IO[Unit]] = exp => IO(exp)
 }
