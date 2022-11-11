b9989bc07567927638bac99caa7b8e23be55cc09
aletullio <74408746+aletullio@users.noreply.github.com>
2022-11-11 16:26:02 +0100

    class GeneClassification

---
+++ src/main/scala/model/dinosaur/GeneClassification.scala
@@ -0,0 +1,8 @@
+package model.dinosaur
+
+private [model] object GeneClassification extends Enumeration {
+
+  type GeneClassification = Value
+  val TEMPERATURE_GENE, AGGRESSION_GENE, REPRODUCTION_GENE, LONGEVITY_GENE,
+  GROWTH_GENE, SPEED_GENE = Value
+}
