5fe89c110e3b5649e3badc587b2b985b19932239
aletullio <74408746+aletullio@users.noreply.github.com>
2022-11-16 18:47:52 +0100

    new population

--- src/main/scala/it/unibo/pps/evoldino/model/dinosaur/Reproduction.scala
+++ src/main/scala/it/unibo/pps/evoldino/model/dinosaur/Reproduction.scala
@@ -1,20 +1,12 @@
 package it.unibo.pps.evoldino.model.dinosaur
 
-import scala.it.unibo.pps.evoldino.model.dinosaur.Dinosaur
+import it.unibo.pps.evoldino.model.dinosaur.GeneClassification
+import it.unibo.pps.evoldino.model.world.WorldSnapshot.Population
+import scala.util.Random
 
 object Reproduction {
-
-  class CoupleGendersException extends ExplainedException()
-
-  type Population = List[Dinosaur]
-
-  case class Couple(mom: Dinosaur, dad: Dinosaur) {
-    def toList: Population = List(mom, dad)
-    if (mom.gender != Female || dad.gender != Male) throw CoupleGendersException()
-  }
-
-  def combineCouples(dinosaurs: Population): List[Couple] = {
-   // val split = dinosaurs partition (_.gender == Female)
-   //  split._1.shuffle zip split._2.shuffle map (c => Couple(c._1, c._2))
+  override def newPopulation: Int = {
+    val Population: Int = GeneClassification.REPRODUCTION_GENE
+    Population/ 2 * 4
   }
 }
---
+++ src/main/scala/it/unibo/pps/evoldino/model/world/WorldSnapshot.scala
@@ -0,0 +1,47 @@
+package it.unibo.pps.evoldino.model.world
+
+import it.unibo.pps.evoldino.model.dinosaur.Dinosaur
+import it.unibo.pps.evoldino.model.world.WorldSnapshot.Population
+import it.unibo.pps.evoldino.model.Disaster
+
+import scala.util.Random
+
+trait WorldSnapshot {
+
+  def environment: Environment
+
+  var population: Population
+
+  val disasters: Seq[Disaster]
+
+  var ended: Boolean
+
+  def damagePopulation(damage: Float): Unit =
+    if (!ended)
+      Random.shuffle(livingPopulation()).take((damage * population.size).toInt).foreach(_.kill())
+    else throw new IllegalArgumentException
+
+  def livingPopulation(): Population = population filter (_.isAlive)
+
+  def closeSnapShot() =
+    ended = true
+  //population flatMap() IMMUTABLE DINOSAURS
+}
+
+object WorldSnapshot {
+
+  type Population = Seq[Dinosaur]
+
+  def apply(
+             environment: Environment,
+             population: Population,
+             disasters: Seq[Disaster] = Seq.empty): WorldSnapshot =
+    new WorldSnapshotImpl(environment, population, disasters, false)
+
+  private class WorldSnapshotImpl(
+                                   override val environment: Environment,
+                                   var population: Population,
+                                   override val disasters: Seq[Disaster],
+                                   var ended: Boolean)
+    extends WorldSnapshot
+}
