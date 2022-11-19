2b1e01ca5bec9023495552187f7ad45c2ac8de19
AlbertoDonati <alberto.donati6@studio.unibo.it>
2022-11-19 06:05:10 +0000

    new branch with disaster ok

--- src/main/scala/it/unibo/pps/evoldino/model/Disaster.scala
+++ src/main/scala/it/unibo/pps/evoldino/model/Disaster.scala
@@ -1,9 +1,14 @@
 package it.unibo.pps.evoldino.model
 
-import it.unibo.pps.evoldino.controller.engine.{ EngineConstants, WorldSnapshot }
+import it.unibo.pps.evoldino.controller.engine.{EngineConstants, WorldSnapshot}
 import it.unibo.pps.evoldino.model.dinosaur.Dinosaur
-import it.unibo.pps.evoldino.model.{ Disaster, Environment }
-
+import it.unibo.pps.evoldino.model.{Disaster, Environment}
+import scala.collection.mutable.ListBuffer
+import cats.implicits.catsSyntaxMonadIdOps
+import math.Ordering.Implicits.infixOrderingOps
+import math.Numeric.Implicits.infixNumericOps
+import math.Fractional.Implicits.infixFractionalOps
+import math.Integral.Implicits.infixIntegralOps
 /*
 import scalafx.geometry.Point2D
 import scalafx.geometry.Point2D.Zero.x
@@ -16,8 +21,11 @@ sealed trait Disaster:
   def name: String
   def damage: Int
   def probability: Int
-  def extension: Int
-  def coordinates: (Int, Int)
+
+  def applyDisasterNEW(d: Disaster, p: List[Dinosaur]): List[Dinosaur] =
+    print("\n nessun disastro applicato \n")
+    p
+
   override def toString: String =
     super.toString +
       "\n name: " + name +
@@ -32,57 +40,98 @@ sealed trait Disaster:
  */
 
 /* DECIDERE COSA ESTENDERE E CHE FARE */
-sealed trait AreaEffect extends Disaster:
+abstract class AreaEffect extends Disaster:
   val extension: Int
   val coordinates: (Int, Int)
 
+  /*FUNZIONANTE
+  override def applyDisasterNEW(d: Disaster, p: List[Dinosaur]): List[Dinosaur] =
+    //val dinoListNew = new ListBuffer[Dinosaur]
+    print("valutazione dinosauri")
+    for (dino <- p)
+      //ma prende quello che gli viene passato o quello su cui si applicat??
+      if(coordinates.equals(dino.testCoordinates))
+        print(dino)
+        print(" !!COLPITO!! \n")
+        dino.testLifePoints = dino.testLifePoints - d.damage
+        //dinoListNew += dino
+      else
+        print(dino)
+    print("\n AreaEffect disastro applicato \n")
+    //dinoListNew.toList
+    p
+  */
+  override def applyDisasterNEW(d: Disaster, p: List[Dinosaur]): List[Dinosaur] =
+    //val dinoListNew = new ListBuffer[Dinosaur]
+    print("valutazione dinosauri")
+    for (dino <- p)
+    //ma prende quello che gli viene passato o quello su cui si applicat??
+      if ((coordinates._1 <= dino.testCoordinates._1 && dino.testCoordinates._1 <= coordinates._1 + extension)
+        && (coordinates._2 <= dino.testCoordinates._2 && dino.testCoordinates._2 <= coordinates._2 + extension))
+          print(dino)
+          print(" !!COLPITO!! \n")
+          dino.testLifePoints = dino.testLifePoints - d.damage
+      //dinoListNew += dino
+      else
+        print(dino)
+    print("\n AreaEffect disastro applicato \n")
+    //dinoListNew.toList
+    p
   override def toString: String =
     super.toString +
       "\n extension " + extension +
       "\n coordinateX " + coordinates._1 +
-      "\n coordinateY " + coordinates._2 +
-      "\n OK :) \n"
+      "\n coordinateY " + coordinates._2
 
-sealed trait ClimateEffect extends Disaster:
+abstract class ClimateEffect extends Disaster:
   val temperature: Int
 
   override def toString: String =
     super.toString +
-      "\n temperature " + temperature +
-      "\n OK"
+      "\n temperature " + temperature
+
+  override def applyDisasterNEW(d: Disaster, p: List[Dinosaur]): List[Dinosaur] =
+    //val dinoListNew = new ListBuffer[Dinosaur]
+    print("valutazione dinosauri")
+
+    for (dino <- p)
+      dino.testLifePoints = dino.testLifePoints - d.damage
+      //dinoListNew += dino
+      print(dino)
+    print("\n AreaEffect disastro applicato \n")
+    //dinoListNew.toList
+    p
 
 object Disaster {
 
   case class Earthquake(e: Int, c: (Int, Int)) extends AreaEffect:
     override val name = "Earthquake"
-    override val damage = 1000
+    override val damage = 20
     override val probability = 5
     override val extension: Int = e
     override val coordinates: (Int, Int) = c
 
   case class Meteorite(e: Int, c: (Int, Int)) extends AreaEffect:
     override val name: String = "Meteorite"
-    override val damage: Int = 1000
+    override val damage: Int = 40
     override val probability: Int = 3
     override val extension: Int = e
     override val coordinates: (Int, Int) = c
 
   case object IceAge extends ClimateEffect:
     override val name: String = "IceAge"
-    override val damage: Int = 40
+    override val damage: Int = 30
     override val probability: Int = 1
     override val temperature: Int = 100
-    override val coordinates: (Int, Int) = (0,0)
-    override val extension: Int = 0
 
   case object Drought extends ClimateEffect:
     override val name: String = "Drought"
-    override val damage: Int = 30
+    override val damage: Int = 10
     override val probability: Int = 7
     override val temperature: Int = 5000
-    override val coordinates: (Int, Int) = (0, 0)
-    override val extension: Int = 0
 }
+
+
 
 /*inutile*/
 /*def testPop(): Unit=
--- src/test/scala/testDemo2.scala
+++ src/test/scala/testDemo2.scala
@@ -6,6 +6,8 @@ import it.unibo.pps.evoldino.model.Disaster.IceAge
 import it.unibo.pps.evoldino.model.Disaster.Drought
 import it.unibo.pps.evoldino.model.AreaEffect
 import it.unibo.pps.evoldino.model.ClimateEffect
+import it.unibo.pps.evoldino.model.dinosaur.Dinosaur
+
 import scala.collection.mutable.ListBuffer
 
 class testDemo2 extends AnyFunSpec:
@@ -15,17 +17,14 @@ class testDemo2 extends AnyFunSpec:
     val population = createDemoDinosaur()
 
     it("should have elements") {
-      print("\n DIS 1 \n")
+      //print("\n DIS 1 \n")
       assertResult(false)(testDisasterSingleRandomProb.equals(()))
-      println(testDisasterSingleRandomProb)
-      println("\n OK DIS \n")
+      //println(testDisasterSingleRandomProb)
+      //println("\n OK DIS \n")
     }
-    it("should have a population") {
-      print("population")
-      assertResult(false)(population.isEmpty)
-    }
-    it("population is changed") {
-
+    it("population is changed Earthquake") {
+      val demoDisEarthquake: AreaEffect = Disaster.Earthquake(e = 10, c = (20, 60))
+      /*
       val demoDisEarthquake: AreaEffect = Disaster.Earthquake(e = 10, c = (20, 60))
       val demoDisMeteorite: AreaEffect = Disaster.Meteorite(e = 0, c = (50, 50))
       val demoDisIceAge: ClimateEffect = Disaster.IceAge
@@ -37,12 +36,39 @@ class testDemo2 extends AnyFunSpec:
       disList += demoDisIceAge
       disList += demoDisDrought
       val disasters = disList.toList
-
+      */
       print("changed population \n")
-
-      val populationNow = applyDisaster(disasters,population)
-      print(populationNow)
+      val populationNow1 = demoDisEarthquake.applyDisasterNEW(demoDisEarthquake,population)
+      //print(populationNow)
       print("changed population end \n")
-      assertResult(false)(population.equals(populationNow))
+      assertResult(false)(population.equals(populationNow1))
+    }
+    it("population is changed Meteorite") {
+      val demoDisMeteorite: AreaEffect = Disaster.Meteorite(e = 0, c = (50, 50))
+      print("changed population Meteorite\n")
+      val populationOld = {
+        new ListBuffer[Dinosaur]
+      }
+      for (din <- population)
+        populationOld += din
+      val populationNow2 = demoDisMeteorite.applyDisasterNEW(demoDisMeteorite, population)
+      print("changed population end \n")
+      assertResult(false)(populationOld.equals(populationNow2))
+    }
+    it("population is changed IceAge") {
+      val demoDisIceAge: ClimateEffect = Disaster.IceAge
+      print("changed population IceAge\n")
+      val populationNow3 = demoDisIceAge.applyDisasterNEW(demoDisIceAge, population)
+      print("changed population end \n")
+
+      assertResult(false)(population.equals(populationNow3))
+    }
+    it("population is changed Drought") {
+      val demoDisDrought: ClimateEffect = Disaster.Drought
+      print("changed population \n")
+      val populationNow4 = demoDisDrought.applyDisasterNEW(demoDisDrought, population)
+      print("changed population end \n")
+
+      assertResult(false)(population.equals(populationNow4))
     }
   }
