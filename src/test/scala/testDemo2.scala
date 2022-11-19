8d83a06aaf60ebef29a83cae9380c211b8304c24
lorenzo-osimani <lorenzo.osimani@studio.unibo.it>
2022-11-19 13:12:27 +0100

    Fixed test

--- src/test/scala/testDemo2.scala
+++ src/test/scala/testDemo2.scala
@@ -13,18 +13,18 @@ import scala.collection.mutable.ListBuffer
 class testDemo2 extends AnyFunSpec:
 
   describe("Tests for apply Disaster") {
+    val populationInitial = createDemoDinosaur()
+    val lf0 = populationInitial(0).testLifePoints
+    val lf1 = populationInitial(1).testLifePoints
+    val lf2 = populationInitial(2).testLifePoints
+    val lf3 = populationInitial(3).testLifePoints
+    val lf4 = populationInitial(4).testLifePoints
+    val lf5 = populationInitial(5).testLifePoints
 
     it("population is changed Earthquake") {
-      val populationInitial = createDemoDinosaur()
-      val lf0 = populationInitial(0).testLifePoints
-      val lf1 = populationInitial(1).testLifePoints
-      val lf2 = populationInitial(2).testLifePoints
-      val lf3 = populationInitial(3).testLifePoints
-      val lf4 = populationInitial(4).testLifePoints
-      val lf5 = populationInitial(5).testLifePoints
 
       val populationDinoEarthquake = createDemoDinosaur()
-      val demoDisEarthquake: AreaEffect = Disaster.Meteorite(e = 10, c = (20, 60))
+      val demoDisEarthquake: AreaEffect = Disaster.Earthquake(e = 10, c = (20, 60))
       print("lifepoints iniziali \n")
       print("colpire dinosauro 3 e 4 \n")
       print("LP 3: " + populationDinoEarthquake(3).testLifePoints + "\n")
@@ -34,93 +34,61 @@ class testDemo2 extends AnyFunSpec:
       print("LP 4: " + populationDisEarthquake(4).testLifePoints + "\n")
       print("changed population end \n")
 
-      print(lf3)
-      print(populationDisEarthquake(3).testLifePoints)
-
       assertResult(true)(lf0.equals(populationDisEarthquake(0).testLifePoints))
       assertResult(true)(lf1.equals(populationDisEarthquake(1).testLifePoints))
       assertResult(true)(lf2.equals(populationDisEarthquake(2).testLifePoints))
       assertResult(false)(lf3.equals(populationDisEarthquake(3).testLifePoints))
       assertResult(false)(lf4.equals(populationDisEarthquake(4).testLifePoints))
-      assertResult(false)(lf5.equals(populationDisEarthquake(5).testLifePoints))
+      assertResult(true)(lf5.equals(populationDisEarthquake(5).testLifePoints))
 
     }
     it("population is changed Meteorite") {
-
       val populationDinoMeteorite = createDemoDinosaur()
-
-      val lf0: Int = populationDinoMeteorite(0).testLifePoints.toInt
-      val lf1 = populationDinoMeteorite(1).testLifePoints
-      val lf2 = populationDinoMeteorite(2).testLifePoints
-      val lf3 = populationDinoMeteorite(3).testLifePoints
-      val lf4 = populationDinoMeteorite(4).testLifePoints
-      val lf5 = populationDinoMeteorite(5).testLifePoints
       val demoDisMeteorite: AreaEffect = Disaster.Meteorite(e = 0, c = (50, 50))
 
       print("lifepoints iniziali \n")
       print("colpire dinosauro 5 \n")
       print("LP 5: " + populationDinoMeteorite(5).testLifePoints + "\n")
       val populationDisMeteorite = demoDisMeteorite.applyDisasterNEW(populationDinoMeteorite)
+      print("LP 5: " + populationDisMeteorite(5).testLifePoints + "\n")
       print("changed population end \n")
 
-      //assertResult(false)(lf0.equals(populationDisMeteorite(0).testLifePoints))
-      /*
-      assertResult(false)(lf1 == populationDisMeteorite(1).testLifePoints)
-      assertResult(false)(lf2 == populationDisMeteorite(2).testLifePoints)
-      assertResult(false)(lf3 == populationDisMeteorite(3).testLifePoints)
-      assertResult(false)(lf4 == populationDisMeteorite(4).testLifePoints)
-      assertResult(true)(lf5 == populationDisMeteorite(5).testLifePoints)
-       */
+      assertResult(true)(lf0 == populationDisMeteorite(0).testLifePoints)
+      assertResult(true)(lf1 == populationDisMeteorite(1).testLifePoints)
+      assertResult(true)(lf2 == populationDisMeteorite(2).testLifePoints)
+      assertResult(true)(lf3 == populationDisMeteorite(3).testLifePoints)
+      assertResult(true)(lf4 == populationDisMeteorite(4).testLifePoints)
+      assertResult(false)(lf5 == populationDisMeteorite(5).testLifePoints)
+
     }
     it("population is changed IceAge") {
       val populationDinoIceAge = createDemoDinosaur()
       val demoDisIceAge: ClimateEffect = Disaster.IceAge
-      print("changed population IceAge\n")
+      print("change population with IceAge \n")
       val populationDisIceAge = demoDisIceAge.applyDisasterNEW(populationDinoIceAge)
       print("changed population end \n")
-      assertResult(false)(
-        populationDinoIceAge(0).testLifePoints == populationDisIceAge(0).testLifePoints
-      )
-      assertResult(false)(
-        populationDinoIceAge(1).testLifePoints == populationDisIceAge(1).testLifePoints
-      )
-      assertResult(false)(
-        populationDinoIceAge(2).testLifePoints == populationDisIceAge(2).testLifePoints
-      )
-      assertResult(false)(
-        populationDinoIceAge(3).testLifePoints == populationDisIceAge(3).testLifePoints
-      )
-      assertResult(false)(
-        populationDinoIceAge(4).testLifePoints == populationDisIceAge(4).testLifePoints
-      )
-      assertResult(false)(
-        populationDinoIceAge(5).testLifePoints == populationDisIceAge(5).testLifePoints
-      )
+
+      assertResult(false)(lf0 == populationDisIceAge(0).testLifePoints)
+      assertResult(false)(lf1 == populationDisIceAge(1).testLifePoints)
+      assertResult(false)(lf2 == populationDisIceAge(2).testLifePoints)
+      assertResult(false)(lf3 == populationDisIceAge(3).testLifePoints)
+      assertResult(false)(lf4 == populationDisIceAge(4).testLifePoints)
+      assertResult(false)(lf5 == populationDisIceAge(5).testLifePoints)
 
     }
     it("population is changed Drought") {
       val populationDinoDrought = createDemoDinosaur()
       val demoDisDrought: ClimateEffect = Disaster.Drought
-      print("changed population \n")
+      print("change population with Drought \n")
       val populationDisDrought = demoDisDrought.applyDisasterNEW(populationDinoDrought)
       print("changed population end \n")
-      assertResult(false)(
-        populationDinoDrought(0).testLifePoints == populationDisDrought(0).testLifePoints
-      )
-      assertResult(false)(
-        populationDinoDrought(1).testLifePoints == populationDisDrought(1).testLifePoints
-      )
-      assertResult(false)(
-        populationDinoDrought(2).testLifePoints == populationDisDrought(2).testLifePoints
-      )
-      assertResult(false)(
-        populationDinoDrought(3).testLifePoints == populationDisDrought(3).testLifePoints
-      )
-      assertResult(false)(
-        populationDinoDrought(4).testLifePoints == populationDisDrought(4).testLifePoints
-      )
-      assertResult(false)(
-        populationDinoDrought(5).testLifePoints == populationDisDrought(5).testLifePoints
-      )
+
+      assertResult(false)(lf0 == populationDisDrought(0).testLifePoints)
+      assertResult(false)(lf1 == populationDisDrought(1).testLifePoints)
+      assertResult(false)(lf2 == populationDisDrought(2).testLifePoints)
+      assertResult(false)(lf3 == populationDisDrought(3).testLifePoints)
+      assertResult(false)(lf4 == populationDisDrought(4).testLifePoints)
+      assertResult(false)(lf5 == populationDisDrought(5).testLifePoints)
+
     }
   }
